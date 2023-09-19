23475b591e1f241e30051aa0f8c9edf009377bf0
Himanshu <prjapathimanshu51@gmail.com>
2023-09-19 10:31:27 +0530

    Fixed navbar

--- components/common/NavBarLight.js
+++ components/common/NavBarLight.js
@@ -31,107 +31,119 @@ const NavbarLight = () => {
   }
 
   return (
-    <nav className="absolute flex items-center justify-between flex-wrap lg:px-40  px-2    py-5  w-full z-10 top-0 ">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <Link
-          className="text-white no-underline hover:text-white hover:no-underline"
-          href="/"
-        >
-          <Image
-            className={"w-16 lg:w-48"}
-            src={Logo}
-            alt={"Logo"}
-            height={150}
-            width={150}
-          />
-        </Link>
-      </div>
-
-      <div className="block lg:hidden">
-        <button
-          id="nav-toggle"
-          onClick={() => {
-            setIsVisible(!isVisible);
-          }}
-          className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600   "
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+      <nav className="absolute flex items-center justify-between flex-wrap lg:px-40  px-2    py-5  w-full z-10 top-0 ">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <Link
+              className="text-white no-underline hover:text-white hover:no-underline"
+              href="/"
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
+            <Image
+                className={"w-16 lg:w-48"}
+                src={Logo}
+                alt={"Logo"}
+                height={150}
+                width={150}
+            />
+          </Link>
+        </div>
 
-      <div
-        className={`w-full flex-grow lg:flex lg:items-center lg:w-auto lg:bg-none  h-screen lg:h-auto  pt-6 lg:pt-0 ${
-          isVisible ? "bg-white fixed top-0 left-0 z-50" : "hidden"
-        }`}
-        id="nav-content"
-      >
-        <RxCross1
-          onClick={() => setIsVisible(false)}
-          size={22}
-          className={"text-right float-right text-green_dark mr-3 lg:hidden"}
-        />
+        <div className="block lg:hidden">
+          <button
+              id="nav-toggle"
+              onClick={() => {
+                setIsVisible(!isVisible);
+              }}
+              className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600   "
+          >
+            <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
 
-        <ul className="list-reset lg:flex justify-end flex-1 items-center text-xl tracking-wider">
-          <li className="mr-3">
-            <Link className="inline-block py-2 px-4  no-underline " href="/">
-              {t("Menu1")}
-            </Link>
-          </li>
-          <li className="mr-3">
-            <Link
-              className="inline-block py-2 px-4  no-underline "
-              href={`/search`}
-            >
-              {t("Menu2")}
-            </Link>
-          </li>
-          <li className="mr-3">
-            <a className="inline-block py-2 px-4  no-underline " href="/#about">
-              {t("Menu3")}
-            </a>
-          </li>
-          <li className="mr-3">
-            <a
-              className="inline-block py-2 px-4  no-underline "
-              href="/#services"
-            >
-              {t("Menu4")}
-            </a>
-          </li>
-          <li className="mr-3">
-            <a className="inline-block py-2 px-4  no-underline " href="/#faq">
-              {t("Menu5")}
-            </a>
-          </li>
+        <div
+            className={`w-full flex-grow lg:flex lg:items-center lg:w-auto lg:bg-none  h-screen lg:h-auto  pt-6 lg:pt-0 ${
+                isVisible ? "bg-white fixed top-0 left-0 z-50" : "hidden"
+            }`}
+            id="nav-content"
+        >
+          <RxCross1
+              onClick={() => setIsVisible(false)}
+              size={22}
+              className={"text-right float-right text-green_dark mr-3 lg:hidden"}
+          />
 
-          <li className="mr-3">
-            <a
-              className="inline-block py-2 px-4  no-underline rounded-full border border-[#158319] "
-              href={"/#contact"}
-            >
-              {t("Menu6")}
-            </a>
-          </li>
-          <li className="mt-4 lg:mt-0 ml-4 flex gap-2 items-center">
-            <Image src={GlobeIcon} alt={"Language Icon"} />
-            <Link className={"text-black"} href="/" locale="en">
-              en
-            </Link>
-            <span className={"h-5 w-0.5 bg-black/60"}></span>
-            <Link className={"text-black/30"} href="/" locale="es">
-              Es
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          <ul className="list-reset lg:flex justify-end flex-1 items-center text-xl tracking-wider">
+            <li className="mr-3">
+              <Link className="inline-block py-2 px-4  no-underline " href="/">
+                {t("Menu1")}
+              </Link>
+            </li>
+            <li className="mr-3">
+              <Link
+                  className="inline-block py-2 px-4  no-underline "
+                  href={`/search`}
+              >
+                {t("Menu2")}
+              </Link>
+            </li>
+            <li className="mr-3">
+              <a className="inline-block py-2 px-4  no-underline " href="/#about">
+                {t("Menu3")}
+              </a>
+            </li>
+            <li className="mr-3">
+              <Link
+                  className="inline-block py-2 px-4  no-underline "
+                  href="/#services"
+                  onClick={() => {
+                    setIsVisible(false);
+                  }}
+              >
+                {t("Menu4")}
+              </Link>
+            </li>
+            <li className="mr-3">
+              <Link  onClick={() => {
+                setIsVisible(false);
+              }} className="inline-block py-2 px-4  no-underline " href="/#faq">
+                {t("Menu5")}
+              </Link>
+            </li>
+
+            <li className="mr-3">
+              <Link
+                  onClick={() => {
+                    setIsVisible(false);
+                  }}
+                  className="inline-block py-2 px-4  no-underline rounded-full border border-[#158319] "
+                  href={"/#contact"}
+              >
+                {t("Menu6")}
+              </Link>
+            </li>
+            <li className="mt-4 lg:mt-0 ml-4 flex gap-2 items-center">
+              <Image src={GlobeIcon} alt={"Language Icon"} />
+              <Link  onClick={() => {
+                setIsVisible(false);
+              }} className={"text-black"} href="/" locale="en">
+                en
+              </Link>
+              <span className={"h-5 w-0.5 bg-black/60"}></span>
+              <Link  onClick={() => {
+                setIsVisible(false);
+              }} className={"text-black/30"} href="/" locale="es">
+                Es
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
   );
 };
 
--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -13,123 +13,139 @@ const Navbar = () => {
   const t = useTranslations("NavBar");
   const pathname = usePathname();
 
- const locale=getLocale(pathname)
+  const locale=getLocale(pathname)
 
   console.log("currentUrl", getLocale(pathname));
   console.log("locale", locale);
 
   return (
-    <nav className="absolute flex items-center justify-between flex-wrap lg:px-40  px-2    py-5  w-full z-10 top-0 ">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <Link
-          className="text-white no-underline hover:text-white hover:no-underline"
-          href="/"
-        >
-          <Image
-            className={"w-16 lg:w-48"}
-            src={"/assets/images/logo.png"}
-            alt={"Logo"}
-            height={150}
-            width={150}
-          />
-        </Link>
-      </div>
-
-      <div className="block lg:hidden">
-        <button
-          id="nav-toggle"
-          onClick={() => {
-            setIsVisible(!isVisible);
-          }}
-          className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
-        >
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title> Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-
-      <div
-        className={`w-full flex-grow lg:flex lg:items-center xl:w-auto lg:bg-none  h-screen xl:h-auto  pt-6 lg:pt-0  ${
-          isVisible ? "bg-green_dark fixed top-0 left-0 z-50" : "hidden"
-        }`}
-        id="nav-content"
-      >
-        <RxCross1
-          onClick={() => setIsVisible(false)}
-          size={22}
-          className={"text-right float-right text-white mr-3 lg:hidden"}
-        />
-        <ul className="list-reset lg:flex justify-end flex-1 items-center text-xl tracking-wider">
-          <li className="mr-3">
-            <Link
-              className="inline-block py-2 px-4 text-white no-underline "
+      <nav className="absolute flex items-center justify-between flex-wrap lg:px-40  px-2    py-5  w-full z-10 top-0 ">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
+          <Link
+              className="text-white no-underline hover:text-white hover:no-underline"
               href="/"
-            >
-              {t("Menu1")}
-            </Link>
-          </li>
-          <li className="mr-3">
-            <Link
-              className="inline-block py-2 px-4 text-white no-underline "
-              href={`/search`}
-            >
-              {t("Menu2")}
-            </Link>
-          </li>
-          <li className="mr-3">
-            <Link
-              className="inline-block py-2 px-4 text-white no-underline "
-              href="#about"
-            >
-              {t("Menu3")}
-            </Link>
-          </li>
-          <li className="mr-3">
-            <Link
-              className="inline-block py-2 px-4 text-white no-underline "
-              href="#services"
-            >
-              {t("Menu4")}
-            </Link>
-          </li>
-          <li className="mr-3">
-            <Link
-              className="inline-block py-2 px-4 text-white no-underline "
-              href="#faq"
-            >
-              {t("Menu5")}
-            </Link>
-          </li>
+          >
+            <Image
+                className={"w-16 lg:w-48"}
+                src={"/assets/images/logo.png"}
+                alt={"Logo"}
+                height={150}
+                width={150}
+            />
+          </Link>
+        </div>
 
-          <li className="mr-3">
-            <a
-              className="inline-block py-2 px-4 text-white no-underline rounded-full border border-[#158319] "
-              href="#contact"
+        <div className="block lg:hidden">
+          <button
+              id="nav-toggle"
+              onClick={() => {
+                setIsVisible(!isVisible);
+              }}
+              className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-white hover:border-white"
+          >
+            <svg
+                className="fill-current h-3 w-3"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
             >
-              {t("Menu6")}
-            </a>
-          </li>
-          <li className="mt-4 lg:mt-0 ml-4 flex gap-2 items-center">
-            <Image src={GlobeIcon} alt={"Language Icon"} />
+              <title> Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
 
-            <Link className={"text-white"} href="/" locale="en">
-              en
-            </Link>
+        <div
+            className={`w-full flex-grow lg:flex lg:items-center xl:w-auto lg:bg-none  h-screen xl:h-auto  pt-6 lg:pt-0  ${
+                isVisible ? "bg-green_dark fixed top-0 left-0 z-50" : "hidden"
+            }`}
+            id="nav-content"
+        >
+          <RxCross1
+              onClick={() => setIsVisible(false)}
+              size={22}
+              className={"text-right float-right text-white mr-3 lg:hidden"}
+          />
+          <ul className="list-reset lg:flex justify-end flex-1 items-center text-xl tracking-wider">
+            <li className="mr-3">
+              <Link
+                  className="inline-block py-2 px-4 text-white no-underline "
+                  href="/"
+              >
+                {t("Menu1")}
+              </Link>
+            </li>
+            <li className="mr-3">
+              <Link
+                  className="inline-block py-2 px-4 text-white no-underline "
+                  href={`/search`}
+              >
+                {t("Menu2")}
+              </Link>
+            </li>
+            <li className="mr-3">
+              <Link
+                  className="inline-block py-2 px-4 text-white no-underline "
+                  href="#about"
+                  onClick={() => {
+                    setIsVisible(false);
+                  }}
+              >
+                {t("Menu3")}
+              </Link>
+            </li>
+            <li className="mr-3">
+              <Link
+                  className="inline-block py-2 px-4 text-white no-underline "
+                  href="#services"
+                  onClick={() => {
+                    setIsVisible(false);
+                  }}
+              >
+                {t("Menu4")}
+              </Link>
+            </li>
+            <li className="mr-3">
+              <Link
+                  className="inline-block py-2 px-4 text-white no-underline "
+                  href="#faq"
+                  onClick={() => {
+                    setIsVisible(false);
+                  }}
+              >
+                {t("Menu5")}
+              </Link>
+            </li>
 
-            <span className={"h-5 w-0.5 bg-white/60"}></span>
-            <Link className={"text-white/30"} href="/" locale="es">
-              Es
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <li className="mr-3">
+              <a
+                  className="inline-block py-2 px-4 text-white no-underline rounded-full border border-[#158319] "
+                  href="#contact"
+                  onClick={() => {
+                    setIsVisible(false);
+                  }}
+              >
+                {t("Menu6")}
+              </a>
+            </li>
+            <li className="mt-4 lg:mt-0 ml-4 flex gap-2 items-center">
+              <Image src={GlobeIcon}  alt={"Language Icon"} />
+
+              <Link className={"text-white"} href="/"  onClick={() => {
+                setIsVisible(false);
+              }} locale="en">
+                en
+              </Link>
+
+              <span className={"h-5 w-0.5 bg-white/60"}></span>
+              <Link  onClick={() => {
+                setIsVisible(false);
+              }} className={"text-white/30"} href="/" locale="es">
+                Es
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
   );
 };
 
